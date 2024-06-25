62de7d9236826154d89e0f6a3a057c39bfc50e18
bhavyae-pokhrel <bhavyaeprasadpokhrel@gmail.com>
2024-06-25 08:11:47 +0530

    time

--- frontend/src/screens/MyOrder.js
+++ frontend/src/screens/MyOrder.js
@@ -59,7 +59,7 @@ export default function MyOrder() {
                                                                     <div className='container w-100 p-0' style={{ height: "65px" }}>
                                                                         <span className='m-1 fs-5'>{arrayData.qty}</span>
                                                                         <span className='m-1 fs-5'>{arrayData.size}</span>
-                                                                        <span className='m-1 '>{time.toLocaleTimeString()}</span>
+                                                                        <span className='m-1 '>{data}</span>
                                                                         <div className=' d-inline ms-2 h-100 w-20 fs-5' >
                                                                             ₹{arrayData.price}/-
                                                                         </div>
