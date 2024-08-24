f5e24e9abf260a37700e189f7a0ec4737519d10f
bhavyae-pokhrel <145043676+bhavyae-pokhrel@users.noreply.github.com>
2024-08-24 21:30:20 +0530

    Update Cart.js

--- frontend/src/screens/Cart.js
+++ frontend/src/screens/Cart.js
@@ -59,7 +59,8 @@ export default function Cart() {
         body: JSON.stringify({
           order_data: data,
           email: userEmail,
-          order_date: new Date().toDateString()
+          // order_date: new Date().toDateString()
+           order_date: new Date()
         })
       });
   
